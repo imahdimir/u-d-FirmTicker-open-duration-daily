c253bd1b938c845a5b0e960a7ae0cf596113ceac
imahdimir <imahdimir@gmail.com>
2022-09-01 18:25:50 +0430

    test

--- testmini.py
+++ testmini.py
@@ -5,34 +5,66 @@
 ##
 
 import sys
+
 from datetime import date
 from datetime import datetime as dt
+from datetime import time
 
 import pandas as pd
 import pyspark.sql.functions as sfunc
 from githubdata import GithubData
 from mirutil.df_utils import read_data_according_to_type as read_data
-from mirutil.df_utils import save_as_prq_wo_index as sprq
 from pyspark.sql import SparkSession
-from pyspark.sql import Window
 from pyspark.sql.functions import concat_ws
-
-from testmini import ColNames
-from testmini import market_end_time
-from testmini import market_start_time
-from testmini import RepoAddresses
-from testmini import Status
-from testmini import status_simplified
+from pyspark.sql import Window
 
 
 spark = SparkSession.builder.getOrCreate()
 
+class RepoAddresses :
+  stch = 'imahdimir/d-clean-d-firm-status-change'
+  twd = 'imahdimir/d-TSE-working-days'
+  i2f = 'imahdimir/d-TSETMC_ID-2-FirmTicker'
+
 ra = RepoAddresses()
+
+class ColNames :
+  jdt = 'JDateTime'
+  jd = 'JDate'
+  d = 'Date'
+  row = 'Row'
+  dt = 'DateTime'
+  id = 'TSETMC_ID'
+  ns = 'NewStatus'
+  iso = 'iso'
+  t = 'Time'
+  trdble = 'Tradable'
+  dup = 'Duplicated'
+  ismktopen = 'IsMarketOpen'
+
 c = ColNames()
+
+class Status :
+  tradeable = True
+  not_tradeable = False
+
 s = Status()
 
-def main() :
+status_simplified = {
+    'مجاز'        : s.tradeable ,
+    'مجاز-محفوظ'  : s.not_tradeable ,
+    'مجاز-متوقف'  : s.not_tradeable ,
+    'ممنوع-متوقف' : s.not_tradeable ,
+    'ممنوع'       : s.not_tradeable ,
+    'ممنوع-محفوظ' : s.not_tradeable ,
+    'مجاز-مسدود'  : s.not_tradeable ,
+    'ممنوع-مسدود' : s.not_tradeable ,
+    }
 
+market_start_time = time(9 , 0 , 0)
+market_end_time = time(12 , 30 , 0)
+
+def main() :
   pass
 
   ##
@@ -75,10 +107,17 @@ def main() :
 
   dti = dti.to_frame()
   dti = dti.reset_index(drop = True)
-
   dti[c.t] = dti[0].dt.time
   dti[c.t] = dti[c.t].astype(str)
-
+  ##
+  dti = dti
+  dtw = pd.DataFrame({
+      c.d : ['2015-06-02'] ,
+      })
+  did = pd.DataFrame({
+      c.id : ['70289374539527245' , '56574323121551263']
+      })
+  ##
   dti[c.ismktopen] = True
   ##
   sdt = spark.createDataFrame(dti)
@@ -90,6 +129,16 @@ def main() :
   ##
   newcol = concat_ws(' ' , sd.Date , sd.Time).alias(c.dt)
   sd = sd.select(c.id , newcol , c.ismktopen)
+  ##
+  dst['1'] = dst[c.dt].astype(str)
+  dst['1'] = dst['1'].str[:10]
+  ##
+  msk = dst[c.id].astype(str).isin(['70289374539527245' , '56574323121551263'])
+  msk &= dst['1'].eq('2015-06-02')
+  len(msk[msk])
+  ##
+  dst = dst[msk]
+  dst = dst[[c.id , c.dt , c.trdble]]
   ##
   sds = spark.createDataFrame(dst)
   ##
@@ -111,17 +160,14 @@ def main() :
   ##
   sd = sd.withColumn(c.d , sfunc.substring(c.dt , 1 , 10))
   ##
+  sdv = sd.toPandas()
+  ##
   sd = sd.groupBy([c.id , c.d]).count()
   ##
   sdv = sd.toPandas()
   ##
-  sprq(sdv , 'temp.prq')
-
-  ##
-
-  rp_stch.rmdir()
-  rp_twd.rmdir()
-  rp_i2f.rmdir()
+  msk = sdv[c.ismktopen].isna()
+  len(msk[msk])
 
   ##
 
