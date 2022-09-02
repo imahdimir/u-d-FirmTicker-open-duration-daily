ded5b8758a54167b20c8c355ea14f588a17c7f9c
imahdimir <imahdimir@gmail.com>
2022-09-02 18:31:09 +0430

    init

--- main.py
+++ main.py
@@ -4,104 +4,219 @@
 
 ##
 
-import sys
+from datetime import time
 
-import pyspark.sql.functions as sfunc
+from githubdata import GithubData
 from mirutil.df_utils import save_as_prq_wo_index as sprq
-from pyspark.sql import SparkSession
-from pyspark.sql import Window
-from pyspark.sql.functions import concat_ws
-
-from shared import _ret_df_of_every_second_in_day
-from shared import _ret_status_change_data
-from shared import _ret_tsetmc_id_data
-from shared import _ret_working_date_data
-from shared import c
+import pandas as pd
+from mirutil.df_utils import read_data_according_to_type as read_data
 
 
-spark = SparkSession.builder.getOrCreate()
+class RepoAddresses :
+  targ = 'imahdimir/d-firm-open-duration-daily'
+  stch = 'imahdimir/d-clean-d-firm-status-change'
+  fip = 'imahdimir/d-firm-possible-trade-spells'
+
+ra = RepoAddresses()
+
+class ColNames :
+  jdt = 'JDateTime'
+  jd = 'JDate'
+  d = 'Date'
+  row = 'Row'
+  dt = 'DateTime'
+  id = 'TSETMC_ID'
+  ns = 'NewStatus'
+  iso = 'iso'
+  t = 'Time'
+  trdble = 'Tradable'
+  dup = 'Duplicated'
+  ismktopen = 'IsMarketOpen'
+  sdt = 'StartDateTime'
+  edt = 'EndDateTime'
+  ndt = 'NextDateTime'
+  dur = 'Duration'
+
+c = ColNames()
+
+class Status :
+  tradeable = True
+  not_tradeable = False
+
+s = Status()
+
+status_simplified = {
+    'مجاز'        : s.tradeable ,
+    'مجاز-محفوظ'  : s.not_tradeable ,
+    'مجاز-متوقف'  : s.not_tradeable ,
+    'ممنوع-متوقف' : s.not_tradeable ,
+    'ممنوع'       : s.not_tradeable ,
+    'ممنوع-محفوظ' : s.not_tradeable ,
+    'مجاز-مسدود'  : s.not_tradeable ,
+    'ممنوع-مسدود' : s.not_tradeable ,
+    }
+
+market_start_time = time(9 , 0 , 0)
+market_end_time = time(12 , 30 , 0)
+
+def add_mktopen_to_dst(dst , dfip) :
+  dst[c.dt] = pd.to_datetime(dst[c.dt])
+  dst[c.d] = dst[c.dt].dt.date
+
+  df1 = dfip[[c.id , c.d , c.sdt , c.edt]]
+
+  for cn in [c.id , c.d] :
+    df1[cn] = df1[cn].astype(str)
+    dst[cn] = dst[cn].astype(str)
+
+  dst = dst.merge(df1 , on = [c.id , c.d] , how = 'left')
+
+  for cn in [c.dt , c.sdt , c.edt] :
+    dst[cn] = pd.to_datetime(dst[cn])
+
+  dst[c.ismktopen] = dst[c.dt].ge(dst[c.sdt])
+  dst[c.ismktopen] &= dst[c.dt].le(dst[c.edt])
+
+  _tfu = lambda x : x.any()
+  _by = [c.id , c.dt]
+  dst[c.ismktopen] = dst.groupby(_by)[c.ismktopen].transform(_tfu)
+
+  dst = dst[[c.id , c.dt , c.ismktopen , c.trdble]]
+
+  dst = dst.drop_duplicates(subset = [c.id , c.dt])
+
+  return dst
 
 def main() :
 
   pass
 
   ##
-  dst = _ret_status_change_data()
-  dtw = _ret_working_date_data()
-  did = _ret_tsetmc_id_data()
+  rp_fip = GithubData(ra.fip)
+  dfip = rp_fip.read_data()
+  dfip.head()
   ##
-  dti = _ret_df_of_every_second_in_day()
+  dfip = dfip[[c.id , c.d , c.sdt , c.edt]]
+  dfip.head()
   ##
-  sdt = spark.createDataFrame(dti)
-  sdw = spark.createDataFrame(dtw)
-  ##
-  for tid in did[c.id] :
-    print(tid)
-
-    did1 = did[did[c.id].eq(tid)]
-
-    sdi = spark.createDataFrame(did1)
-
-    sd = sdi.crossJoin(sdw)
-    sd = sd.crossJoin(sdt)
-
-    newcol = concat_ws(' ' , sd.Date , sd.Time).alias(c.dt)
-    sd = sd.select(c.id , newcol , c.ismktopen)
-
-    sds = spark.createDataFrame(dst)
-
-    sd = sd.join(sds , [c.id , c.dt] , how = 'outer')
-
-    sd = sd.sort([c.id , c.dt])
-
-    window = Window.partitionBy(c.id).orderBy(c.dt).rowsBetween(-sys.maxsize ,
-                                                                0)
-    filled_column = sfunc.last(sd[c.trdble] , ignorenulls = True).over(window)
-    sdf_filled = sd.withColumn('filled' , filled_column)
-
-    sd = sdf_filled
-
-    msk = sd['filled'] == True
-    msk &= sd[c.ismktopen] == True
-    sd = sd.filter(msk)
-
-    sd = sd.select(c.id , c.dt)
-
-    sd = sd.withColumn(c.d , sfunc.substring(c.dt , 1 , 10))
-
-    sd = sd.groupBy([c.id , c.d]).count()
-
-    sdv = sd.toPandas()
-
-    sprq(sdv , f'{tid}.prq')
-
-    break
-
+  dfip[c.id] = dfip[c.id].astype(str)
   ##
 
 
   ##
-
-
+  rp_stch = GithubData(ra.stch)
+  dst = rp_stch.read_data()
+  ##
+  dst[c.trdble] = dst[c.ns].map(status_simplified)
+  ##
+  dst = dst[[c.id , c.dt , c.trdble]]
+  dst.head()
+  ##
+  dst[c.id] = dst[c.id].astype(str)
+  ##
+  dst = add_mktopen_to_dst(dst , dfip)
+  dst.head()
   ##
 
 
   ##
+  dfip1 = dfip[[c.id]]
+  dfip1[[c.dt]] = dfip[[c.sdt]]
 
+  dfip2 = dfip[[c.id]]
+  dfip2[[c.dt]] = dfip[[c.edt]]
+  ##
+  dfip = pd.concat([dfip1 , dfip2])
 
+  del dfip1
+  del dfip2
+  ##
+  dfip[c.ismktopen] = True
+  ##
+  dfip.head()
+  ##
+
+  dfip[c.dt] = dfip[c.dt].astype(str)
+  dst[c.dt] = dst[c.dt].astype(str)
+  ##
+  df = dfip.merge(dst , on = [c.id , c.dt] , how = 'outer')
+  df.head()
+
+  ##
+  del dfip
+  ##
+
+  msk = df.duplicated(subset = [c.id , c.dt] , keep = False)
+  msk &= df[c.trdble].isna()
+
+  df1 = df[msk]
+  df = df[~ msk]
+  ##
+
+  msk = df[c.ismktopen + '_x'].isna()
+  print(len(msk[msk]))
+
+  df.loc[msk , c.ismktopen + '_x'] = df[c.ismktopen + '_y']
+
+  ##
+  df.drop(columns = c.ismktopen + '_y' , inplace = True)
+
+  df.rename(columns = {
+      c.ismktopen + '_x' : c.ismktopen
+      } , inplace = True)
+  ##
+  df.head()
+  ##
+
+  df = df.sort_values(by = [c.id , c.dt])
+  df.head()
+  ##
+  df[c.trdble] = df.groupby([c.id])[c.trdble].ffill()
+  ##
+  msk = df[c.ismktopen]
+  msk &= df[c.trdble].notna()
+
+  df1 = df[~ msk]
+
+  df = df[msk]
+  ##
+  df.head()
   ##
 
 
   ##
+  df.drop(columns = c.ismktopen , inplace = True)
+  ##
+  df[c.d] = pd.to_datetime(df[c.dt]).dt.date
 
+  ##
+  df[c.ndt] = df.groupby([c.id , c.d])[c.dt].shift(-1)
+  df.head()
+
+  ##
+  _col = pd.to_datetime(df[c.ndt]) - pd.to_datetime(df[c.dt])
+  _col = _col.dt.seconds
+  ##
+  msk = df[c.trdble]
+
+  df.loc[msk , c.dur] = _col[msk]
+
+  df.head()
+  ##
+  df1 = df.groupby([c.id , c.d])[c.dur].sum()
+  df1.head()
+  ##
+  df1 = df1.to_frame()
+  df1.reset_index(inplace = True)
+
+  ##
+  df1[c.dur] = df1[c.dur].astype(int)
+  ##
+  df1.head()
 
   ##
 
-
   ##
-
-##
-
 
 ##
 
@@ -111,14 +226,5 @@ if __name__ == '__main__' :
   print('done')
 
 ##
-import dulwich
-
-
-##
-url = 'https://github.com/imahdimir/d-clean-d-firm-status-change'
-dulwich.porcelain.clone(url)
-
-##
-
 
 ##
