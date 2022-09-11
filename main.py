51a6db6797617666f5c56277fdd69e552c537e52
imahdimir <imahdimir@gmail.com>
2022-09-11 21:52:03 +0430

    v2

--- main.py
+++ main.py
@@ -2,249 +2,235 @@
 
   """
 
-##
-
-from datetime import time
+import json
 
 import pandas as pd
 from githubdata import GithubData
-from mirutil import utils as mu
 from mirutil.df_utils import save_as_prq_wo_index as sprq
+from mirutil.jdate import vect_make_datetime_from_iso_jdate_time as vjd
+from persiantools.jdatetime import JalaliDate
 
 
-class RepoAddresses :
-  targ = 'https://github.com/imahdimir/d-firm-open-duration-daily'
-  stch = 'https://github.com/imahdimir/d-clean-d-firm-status-change'
-  fip = 'https://github.com/imahdimir/d-firm-possible-trade-spells'
-  cur_url = 'https://github.com/imahdimir/b-d-firm-open-duration-daily'
+class GDUrl :
+    with open('gdu.json' , 'r') as fi :
+        gj = json.load(fi)
 
-ra = RepoAddresses()
+    selff = gj['selff']
+    src = gj['src']
+    src0 = gj['src0']
+    trg = gj['trg']
+
+gu = GDUrl()
 
 class ColNames :
-  d = 'Date'
-  dt = 'DateTime'
-  id = 'TSETMC_ID'
-  ns = 'NewStatus'
-  t = 'Time'
-  trdble = 'Tradable'
-  ismktopen = 'IsMarketOpen'
-  sdt = 'StartDateTime'
-  edt = 'EndDateTime'
-  ndt = 'NextDateTime'
-  dur = 'Duration'
+    d = 'Date'
+    jdt = 'JDateTime'
+    dt = 'DateTime'
+    sjdt = 'StartJDateTime'
+    ejdt = 'EndJDateTime'
+    ns = 'NewStatus'
+    trdble = 'Tradable'
+    ismktopen = 'IsMarketOpen'
+    sdt = 'StartDateTime'
+    edt = 'EndDateTime'
+    ndt = 'NextDateTime'
+    dur = 'Duration'
+    ftic = 'FirmTicker'
+    jd = 'JDate'
 
 c = ColNames()
 
 class Status :
-  tradeable = True
-  not_tradeable = False
+    tradeable = True
+    not_tradeable = False
 
 s = Status()
 
 status_simplified = {
-    'مجاز'        : s.tradeable ,
-    'مجاز-محفوظ'  : s.not_tradeable ,
-    'مجاز-متوقف'  : s.not_tradeable ,
-    'ممنوع-متوقف' : s.not_tradeable ,
-    'ممنوع'       : s.not_tradeable ,
-    'ممنوع-محفوظ' : s.not_tradeable ,
-    'مجاز-مسدود'  : s.not_tradeable ,
-    'ممنوع-مسدود' : s.not_tradeable ,
-    }
-
-market_start_time = time(9 , 0 , 0)
-market_end_time = time(12 , 30 , 0)
-
-def add_mktopen_to_dst(dst , dfip) :
-  dst[c.dt] = pd.to_datetime(dst[c.dt])
-  dst[c.d] = dst[c.dt].dt.date
-
-  df1 = dfip[[c.id , c.d , c.sdt , c.edt]]
-
-  for cn in [c.id , c.d] :
-    df1[cn] = df1[cn].astype(str)
-    dst[cn] = dst[cn].astype(str)
-
-  dst = dst.merge(df1 , on = [c.id , c.d] , how = 'left')
-
-  for cn in [c.dt , c.sdt , c.edt] :
-    dst[cn] = pd.to_datetime(dst[cn])
-
-  dst[c.ismktopen] = dst[c.dt].ge(dst[c.sdt])
-  dst[c.ismktopen] &= dst[c.dt].le(dst[c.edt])
-
-  _tfu = lambda x : x.any()
-  _by = [c.id , c.dt]
-  dst[c.ismktopen] = dst.groupby(_by)[c.ismktopen].transform(_tfu)
-
-  dst = dst[[c.id , c.dt , c.ismktopen , c.trdble]]
-
-  dst = dst.drop_duplicates(subset = [c.id , c.dt])
-
-  return dst
+        'مجاز'        : s.tradeable ,
+        'مجاز-محفوظ'  : s.not_tradeable ,
+        'مجاز-متوقف'  : s.not_tradeable ,
+        'ممنوع-متوقف' : s.not_tradeable ,
+        'ممنوع'       : s.not_tradeable ,
+        'ممنوع-محفوظ' : s.not_tradeable ,
+        'مجاز-مسدود'  : s.not_tradeable ,
+        'ممنوع-مسدود' : s.not_tradeable ,
+        }
 
 def main() :
+    pass
 
-  pass
+    ##
 
-  ##
-  rp_fip = GithubData(ra.fip)
-  dfip = rp_fip.read_data()
-  dfip.head()
-  ##
-  dfip = dfip[[c.id , c.d , c.sdt , c.edt]]
-  dfip.head()
-  ##
-  dfip[c.id] = dfip[c.id].astype(str)
-  ##
+    gds = GithubData(gu.src)
+    gds.overwriting_clone()
+    ##
+    ds = gds.read_data()
+    ##
+    ds[c.trdble] = ds[c.ns].map(status_simplified)
+    ##
+    ds = ds[[c.ftic , c.jdt , c.trdble]]
+    ##
+    ds[c.dt] = ds[c.jdt].apply(vjd)
+    ds = ds.drop(columns = c.jdt)
+    ##
+
+    gd0 = GithubData(gu.src0)
+    gd0.overwriting_clone()
+    d0 = gd0.read_data()
+    d0.head()
+    ##
+
+    d0[c.sdt] = d0[c.sjdt].apply(vjd)
+    d0[c.edt] = d0[c.ejdt].apply(vjd)
+
+    d0 = d0.drop(columns = [c.sjdt , c.ejdt])
+
+    d0v = d0.head()
+    ##
+
+    ds[c.d] = ds[c.dt].dt.date
+    ##
+    d0[c.d] = d0[c.sdt].dt.date
+    ##
+
+    ds = ds.merge(d0 , on = [c.ftic , c.d] , how = 'left')
+    ##
+    ds[c.ismktopen] = ds[c.dt].ge(ds[c.sdt])
+    ds[c.ismktopen] &= ds[c.dt].le(ds[c.edt])
+
+    ##
+    fu1 = lambda x : x.any()
+    by = [c.ftic , c.dt]
+    ds[c.ismktopen] = ds.groupby(by)[c.ismktopen].transform(fu1)
+
+    ##
+    ds = ds.drop(columns = [c.sdt , c.edt , c.d])
+
+    ##
+
+    d1 = d0[[c.ftic]]
+    d1[c.dt] = d0[c.sdt]
+
+    d2 = d0[[c.ftic]]
+    d2[c.dt] = d0[[c.edt]]
+    ##
+    d3 = pd.concat([d1 , d2])
+    ##
+    del d1 , d2
+    ##
+
+    d3[c.ismktopen] = True
+    ##
+    d3.head()
+
+    ##
+
+    df = d3.merge(ds , on = [c.ftic , c.dt] , how = 'outer')
+    ##
+    dfv = df.head()
+    ##
+    del d3
+    ##
+
+    msk = df.duplicated(subset = [c.ftic , c.dt] , keep = False)
+    msk &= df[c.trdble].isna()
+
+    df1 = df[msk]
+    df = df[~ msk]
+    ##
+
+    msk = df[c.ismktopen + '_x'].isna()
+    print(len(msk[msk]))
+
+    df.loc[msk , c.ismktopen + '_x'] = df[c.ismktopen + '_y']
+
+    ##
+    df = df.drop(columns = c.ismktopen + '_y')
+
+    ren = {
+            c.ismktopen + '_x' : c.ismktopen ,
+            }
+    df = df.rename(columns = ren)
+    ##
+    dfv = df.head()
+    ##
+
+    df = df.sort_values(by = [c.ftic , c.dt])
+    dfv = df.head()
+    ##
+    df[c.trdble] = df.groupby([c.ftic])[c.trdble].ffill()
+
+    ##
+
+    msk = df[c.ismktopen]
+    msk &= df[c.trdble].notna()
+
+    df1 = df[~ msk]
+    df = df[msk]
+
+    ##
+
+    df = df.drop(columns = c.ismktopen)
+    ##
+
+    df[c.d] = pd.to_datetime(df[c.dt]).dt.date
+    ##
+    df[c.ndt] = df.groupby([c.ftic , c.d])[c.dt].shift(-1)
+    dfv = df.head()
+
+    ##
+
+    ncol = pd.to_datetime(df[c.ndt]) - pd.to_datetime(df[c.dt])
+    ncol = ncol.dt.seconds
+    ##
+    msk = df[c.trdble]
+    df.loc[msk , c.dur] = ncol[msk]
+
+    dfv = df.head()
+    ##
+
+    df1 = df.groupby([c.ftic , c.d])[c.dur].sum()
+    ##
+    df1 = df1.to_frame()
+    df1 = df1.reset_index()
+    ##
+    df1[c.dur] = df1[c.dur].astype(int)
+    ##
+    df1v = df1.head(1000)
+    ##
+
+    df1[c.jd] = df1[c.d].apply(lambda x : JalaliDate(x).isoformat())
+
+    ##
+    df1 = df1[[c.ftic , c.jd , c.dur]]
+    df1v = df1.head(1000)
+
+    ##
+
+    gdt = GithubData(gu.trg)
+    gdt.overwriting_clone()
+
+    ##
+    sprq(df1 , gdt.data_fp)
+
+    ##
+    msg = 'builded by: '
+    msg += gu.selff
+    ##
+
+    gdt.commit_and_push(msg)
+
+    ##
+
+    gds.rmdir()
+    gd0.rmdir()
+    gdt.rmdir()
 
 
-  ##
-  rp_stch = GithubData(ra.stch)
-  dst = rp_stch.read_data()
-  ##
-  dst[c.trdble] = dst[c.ns].map(status_simplified)
-  ##
-  dst = dst[[c.id , c.dt , c.trdble]]
-  dst.head()
-  ##
-  dst[c.id] = dst[c.id].astype(str)
-  ##
-  dst = add_mktopen_to_dst(dst , dfip)
-  dst.head()
-  ##
-
-
-  ##
-  dfip1 = dfip[[c.id]]
-  dfip1[[c.dt]] = dfip[[c.sdt]]
-
-  dfip2 = dfip[[c.id]]
-  dfip2[[c.dt]] = dfip[[c.edt]]
-  ##
-  dfip = pd.concat([dfip1 , dfip2])
-
-  del dfip1
-  del dfip2
-  ##
-  dfip[c.ismktopen] = True
-  ##
-  dfip.head()
-  ##
-
-  dfip[c.dt] = dfip[c.dt].astype(str)
-  dst[c.dt] = dst[c.dt].astype(str)
-  ##
-  df = dfip.merge(dst , on = [c.id , c.dt] , how = 'outer')
-  df.head()
-
-  ##
-  del dfip
-  ##
-
-  msk = df.duplicated(subset = [c.id , c.dt] , keep = False)
-  msk &= df[c.trdble].isna()
-
-  df1 = df[msk]
-  df = df[~ msk]
-  ##
-
-  msk = df[c.ismktopen + '_x'].isna()
-  print(len(msk[msk]))
-
-  df.loc[msk , c.ismktopen + '_x'] = df[c.ismktopen + '_y']
-
-  ##
-  df.drop(columns = c.ismktopen + '_y' , inplace = True)
-
-  df.rename(columns = {
-      c.ismktopen + '_x' : c.ismktopen
-      } , inplace = True)
-  ##
-  df.head()
-  ##
-
-  df = df.sort_values(by = [c.id , c.dt])
-  df.head()
-  ##
-  df[c.trdble] = df.groupby([c.id])[c.trdble].ffill()
-  ##
-  msk = df[c.ismktopen]
-  msk &= df[c.trdble].notna()
-
-  df1 = df[~ msk]
-
-  df = df[msk]
-  ##
-  df.head()
-  ##
-
-
-  ##
-  df.drop(columns = c.ismktopen , inplace = True)
-  ##
-  df[c.d] = pd.to_datetime(df[c.dt]).dt.date
-
-  ##
-  df[c.ndt] = df.groupby([c.id , c.d])[c.dt].shift(-1)
-  df.head()
-
-  ##
-  _col = pd.to_datetime(df[c.ndt]) - pd.to_datetime(df[c.dt])
-  _col = _col.dt.seconds
-  ##
-  msk = df[c.trdble]
-
-  df.loc[msk , c.dur] = _col[msk]
-
-  df.head()
-  ##
-  df1 = df.groupby([c.id , c.d])[c.dur].sum()
-  df1.head()
-  ##
-  df1 = df1.to_frame()
-  df1.reset_index(inplace = True)
-
-  ##
-  df1[c.dur] = df1[c.dur].astype(int)
-  ##
-  df1.head()
-
-  ##
-
-
-  ##
-  rp_targ = GithubData(ra.targ)
-  rp_targ.clone()
-
-  ##
-  sprq(df1 , rp_targ.data_fp)
-
-  ##
-  tokp = '/Users/mahdi/Dropbox/tok.txt'
-  tok = mu.get_tok_if_accessible(tokp)
-  ##
-  msg = 'builded by: '
-  msg += ra.cur_url
-  ##
-  rp_targ.commit_and_push(msg , user = rp_targ.user_name , token = tok)
-
-  ##
-
-  rp_stch.rmdir()
-  rp_fip.rmdir()
-  rp_targ.rmdir()
-
-  ##
-
-
-  ##
+    ##
 
 ##
 if __name__ == '__main__' :
-  main()
-  print('done')
-
-##
-
-##
+    main()
+    print('done')
